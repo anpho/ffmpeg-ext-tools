e8001c5be3c41340c348100b5ffb852c151dc3ea
Merrick Zhang <anphorea@users.noreply.github.com>
2017-07-22 23:29:15 +0800

    Update trimstart.cpp
    accept multiple files to batch process.

--- trimstart/trimstart.cpp
+++ trimstart/trimstart.cpp
@@ -1,10 +1,81 @@
 #include <iostream>
 #include <string>
 #include <stdlib.h>
+#include <vector>
 
 using namespace std;
 static int PARAM_NOT_SPECIFIED = 1;
 static int INVALID_INPUT = 2;
+static int UNKNOWN_ERROR = 9;
+
+string _current_dir="", _exe_name="";
+
+int install() {
+	cout << "Install to System Context Menu. Please specify the .reg file." << endl;
+	return 0;
+}
+
+int uninstall() {
+	return 0;
+}
+void show_readme() {
+	cout << "Trimstart Tool for ffmpeg." << endl;
+	cout << "Merrick Zhang ( anphorea@gmail.com )" << endl;
+	cout << "====================================" << endl;
+	cout << "Usage:" << endl;
+	cout << "Create a shortcut in your shell:sendto folder, select file(s) to trim, use SendTo to invoke this tool." << endl;
+	cout << "OR" << endl;
+	cout << _exe_name<<" [media-file]\tTrim the specified media file." << endl;
+	//cout << _exe_name<<" /i \t \tInstall to system context menu." << endl;
+	//cout << _exe_name<<" /u \t \tUninstall." << endl;
+}
+
+bool setup_path_var(const string& argv1) {
+	if ( argv1.empty()) return false;
+
+	int slash_pos = argv1.find_last_of("\\");
+	if (slash_pos >> 0) {
+		_current_dir = argv1.substr(0, slash_pos + 1);
+		_exe_name = argv1.substr(slash_pos + 1);
+	}
+	else {
+		cerr << "Strange Parameter: " + argv1 << endl;
+		return false;
+	}
+	return true;
+}
+char full[_MAX_PATH];
+/*
+ * Get the full path of this program.
+ */
+char* getFullPath(char * partialPath)
+{
+	if (_fullpath(full, partialPath, _MAX_PATH) != NULL){
+		return full;
+	}
+	else
+		return "";
+}
+
+
+void trim_file(const string& mediafile, const string trim_start_seconds) {
+	cout << "Processing:\t" << mediafile << endl;
+	string new_file_name(mediafile);
+	int pos = new_file_name.find_last_of('.');
+	new_file_name.insert(pos, "-trim");
+	cout << "New Filename: " << new_file_name << endl;
+
+	string cmd = "ffmpeg -ss ";
+	cmd.append(trim_start_seconds);
+	cmd.append(" -i ");
+	cmd.append(mediafile);
+	cmd.append(" -codec copy ");
+	cmd.append(new_file_name);
+
+	cout << "Final cmd line: " << cmd << endl;
+
+	system(cmd.c_str());
+}
 
 /*
  * This program is used to generate the command line for ffmpeg tool suite,
@@ -13,44 +84,45 @@ static int INVALID_INPUT = 2;
  */
 int main(int argc, char *argv[])
 {
-	//set system variables;
-	string pathv = getenv("PATH");
-	cout<<"PATH:"<<pathv<<endl;
-	// not done.
+	// setup path variables
+	string app_path(getFullPath(argv[0]));
+	if (!setup_path_var(app_path)) {
+		return UNKNOWN_ERROR;
+	}
 
-    if (argc ==1){
-        cout<<"Usage:"<<endl;
-        cout<<"    ffmpeg-ext.exe [media-file-path]"<<endl;
-        cout<<"    [media-file-path] must be specified."<<endl;
-		cout<<"Return Code:"<<endl;
-		cout<<"\t1\tParam_Not_Specified"<<endl;
-		cout<<"\t2\tInvalid_Input"<<endl;
-        return PARAM_NOT_SPECIFIED;
-    }
+	vector<string> mediafiles;
 
-    string filepath = argv[1];
-    cout<<"Param[media-file-path] is : "<<filepath<<endl;
+	if (argc > 1) {
+		if (strcmp(argv[1], "/i") == 0) {
+			return install();
+		}
+		else if (strcmp(argv[1], "/u") == 0) {
+			return uninstall();
+		}
+		else {
+			// To trim
+			mediafiles.clear();
+			for (int i = 1; i < argc; i++) {
+				mediafiles.push_back(argv[i]);
+			}
+		}
+	}
+	else {
+		return PARAM_NOT_SPECIFIED;
+	}
+
+	cout << "Trimstart tool for ffmpeg" << endl << endl;
 
     string start_seconds;
-    cout<< "Trim start (seconds):";
+    cout<< "Trim (seconds) from the beginning (Enter decimal number or hh:mm:ss): ";
     cin>> start_seconds;
 
-    string new_file_name;
-    int pos=filepath.find_last_of('.');
-    filepath.insert(pos,"-trim");
-    new_file_name = filepath;
-    cout<<"Use filename: "<<filepath<<endl;
+	vector<string>::iterator it;
+	for (it = mediafiles.begin(); it != mediafiles.end(); it++) {
+		trim_file(*it, start_seconds);
+	}
 
-    string cmd="ffmpeg -ss ";
-    cmd.append(start_seconds);
-    cmd.append(" -i ");
-    cmd.append(argv[1]);
-    cmd.append(" -codec copy ");
-    cmd.append(new_file_name);
-
-    cout<<"Final cmd line: "<<cmd<<endl;
-
-    system(cmd.c_str());
 	
     return 0;
 }
+
